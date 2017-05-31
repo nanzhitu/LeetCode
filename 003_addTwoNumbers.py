345f734654e879f7f884330fce6b975ac516bd7f
neo-gj.ye <neo-gj.ye@mstarsemi.com>
2017-05-31 22:50:33 +0800

    add 003_addTwoNumbers

---
+++ 003_addTwoNumbers.py
@@ -0,0 +1,54 @@
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+def addTwoNumbers( l1, l2):   
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    sign = 0
+    Sum = 0
+    n = ListNode(0)
+    r = n
+    while True:
+        Sum = 0
+        if l1 != None:
+            Sum += l1.val
+            l1 = l1.next
+        if l2 != None:
+            Sum += l2.val
+            l2 = l2.next
+        Sum += sign
+        n.next = ListNode(Sum % 10)
+        n = n.next
+        sign = Sum / 10
+        if l1 == None and l2 == None and sign ==0:
+            break
+    return r.next
+
+l1 = ListNode(4)
+i1 = ListNode(5)
+i2 = ListNode(8)
+
+l1.next = i1
+i1.next = i2
+
+l2 = ListNode(3)
+j1 = ListNode(5)
+j2 = ListNode(1)
+
+l2.next = j1
+j1.next = j2
+
+l3 = addTwoNumbers(l1, l2)
+
+while l3 != None:
+    print l3.val
+    l3 = l3.next
+
+
+
